dca6ba2cbd78ce648c95b0a3a7af3c7f32c49470
poojaiyengar123 <62581537+poojaiyengar123@users.noreply.github.com>
2023-10-22 12:08:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pydeck as pdk
 from PIL import Image
 import requests
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import datetime
 
 logo = Image.open("logo.png")
