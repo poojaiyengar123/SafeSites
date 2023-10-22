6610819c995ffe1e0a63170a4ead18530d001cfa
nkhur <145411496+nkhur@users.noreply.github.com>
2023-10-22 11:34:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pydeck as pdk
 from PIL import Image
 import requests
-from beautifulsoup4 import BeautifulSoup
+from beautifulsoup import BeautifulSoup
 import re
 import datetime
 
